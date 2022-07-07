8a66f5818419f7a760ab9c9007e019e4bfc92815
Daniel Bachar <danibachar89@gmail.com>
2022-07-07 01:51:59 -0600

    Update duration and response time with a scale option

--- response_time_function.py
+++ response_time_function.py
@@ -1,24 +1,24 @@
-def response_time_function(job_type):
+def response_time_function(job_type, scale = 1):
     if job_type == "product_page":
-        return 0.006, 0.0#0.22
+        return 0.006 / scale, 0.0#0.22
     if job_type == "user_page":
-        return 0.006, 0.0#0.22
+        return 0.006 / scale, 0.0#0.22
     if job_type == "dashboard":
-        return 0.006, 0.0#0.22
+        return 0.006 / scale, 0.0#0.22
     if job_type == "review":
-        return 0.01, 0.0#0.22
+        return 0.01 / scale, 0.0#0.22
     if job_type == "details":
-        return 0.009, 0.0#0.22
+        return 0.009 / scale, 0.0#0.22
     if job_type == "rating":
-        return 0.016, 0.0# 0.22
+        return 0.016 / scale, 0.0# 0.22
     if job_type == "main_db":
-        return 0.021, 0.0#0.22
+        return 0.021 / scale, 0.0#0.22
     if job_type == "image_server":
-        return 0.006, 0.0#0.22
+        return 0.006 / scale, 0.0#0.22
     if job_type == "storage":
-        return 0.026, 0.0
+        return 0.026 / scale, 0.0
 
-def duration_by(load, type):
-    slope, b = response_time_function(type)
+def duration_by(load, type, scale):
+    slope, b = response_time_function(type, scale)
     duration_in_seconds = (slope * load + b)
     return duration_in_seconds
